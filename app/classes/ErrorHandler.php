d922b2ac3b0d446dcf21436278efd91d169743b5
Gentry Rolofson <xdarkicex@gmail.com>
2017-03-01 22:09:30 -0800

    commit validation half done

--- app/classes/ErrorHandler.php
+++ app/classes/ErrorHandler.php
@@ -2,5 +2,24 @@
 
 class ErrorHandler
 {
-  
+  protected $errors;
+  public function add_error($error, $key = null)
+  {
+    if($key)
+    {
+      $this->errors[$key][] = $error;
+    }
+    else
+    {
+      $this->errors[] = $error;
+    }
+  }
+  public function has_errors()
+  {
+    return count($this->all()) ? true : false;
+  }
+  public function all()
+  {
+    
+  }
 }
